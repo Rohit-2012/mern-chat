e2e7faf878f8cf391d5adaa4d6e92f17e28139f9
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-19 20:58:03 +0530

    add signup feature

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,13 +1,60 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
 export const signup = async (req, res) => {
-    try {
-        const {fullName, username, password, confirmPassword, gender} = req.body
-    } catch (error) {
-        
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password don't match" });
     }
-}
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // HASH PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+      if (newUser) {
+        // GENERATE JWT
+      generateTokenAndSetCookie(newUser._id, res)
+
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        gender: newUser.gender,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 export const login = (req, res) => {
-    console.log("Login User")
-}
+  console.log("Login User");
+};
 export const logout = (req, res) => {
-    console.log("Logout User")
-}
+  console.log("Logout User");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
-const app = express()
 
 import authRouter from './routes/auth.routes.js'
 import connectToMongoDB from './db/conntectToMongoDB.js'
 
+const app = express()
 const PORT = process.env.PORT || 8000
+
+app.use(express.json())
 
 app.use("/api/auth", authRouter)
 
