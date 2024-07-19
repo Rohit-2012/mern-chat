654fa5fd3577cc6b1640c0c5545787e768bc2dcc
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-19 21:12:07 +0530

    add logout endpoint

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
+// SIGNUP CONTROLLER
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
@@ -53,7 +54,7 @@ export const signup = async (req, res) => {
   }
 };
 
-
+// LOGIN CONTROLLER
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body
@@ -74,12 +75,18 @@ export const login = async (req, res) => {
     })
 
   } catch (error) {
-    console.log("Error in signup controller", error.message);
+    console.log("Error in login controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-
+// LOGOUT CONTROLLER
 export const logout = (req, res) => {
-  console.log("Logout User");
+  try {
+    res.cookie("jwt", "", {maxAge: 0})
+    res.status(200).json({message: "Logged out successfully"})
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
