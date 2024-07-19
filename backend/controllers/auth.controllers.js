201c9c86b28e89b0cdf0d66b5b5e50eb0bc2d8f9
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-19 21:07:08 +0530

    add login controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -52,9 +52,34 @@ export const signup = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-export const login = (req, res) => {
-  console.log("Login User");
+
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password || "")
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({error: "Invalid username or password"})
+    }
+
+    generateTokenAndSetCookie(user._id, res)
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic
+    })
+
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
+
 export const logout = (req, res) => {
   console.log("Logout User");
 };
