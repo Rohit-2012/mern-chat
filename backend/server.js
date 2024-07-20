5aafb6e392aa8e87709f0abd90dfa4bd3f21e5dc
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-20 14:31:57 +0530

    add send message endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 import 'dotenv/config'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 import authRouter from './routes/auth.routes.js'
+import messageRouter from './routes/message.routes.js'
 import connectToMongoDB from './db/conntectToMongoDB.js'
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
+// MIDDLEWARES
 app.use(express.json())
+app.use(cookieParser())
 
+// ROUTES
 app.use("/api/auth", authRouter)
+app.use("/api/messages", messageRouter)
 
 
 app.listen(PORT, () => {
