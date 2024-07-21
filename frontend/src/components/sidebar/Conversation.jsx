559712db6b783696a7ee3c6d400e862c89776546
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-21 19:59:00 +0530

    get conversations

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,22 +1,27 @@
-const Conversation = () => {
+import useConversation from '../../zustand/useConversation'
+
+const Conversation = ({ conversation, emoji, lastIndex }) => {
+    const { selectedConversation, setSelectedConversation } = useConversation()
+    
+    const isSelected = selectedConversation?._id === conversation._id
     return (
         <>
-            <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+            <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected ? "bg-sky-500" : ""}`} onClick={() => setSelectedConversation(conversation)}>
                 <div className="avatar online">
                     <div className="w-12 rounded-full">
-                        <img src="https://avatar.iran.liara.run/public/girl" alt="user avatar" />
+                        <img src={conversation.profilePic} alt="user avatar" />
                     </div>
                 </div>
 
                 <div className="flex flex-col flex-1">
                     <div className="flex gap-3 justify-between">
-                        <p className="font-bold text-gray-200">Chelci Chahar</p>
-                        <span className="text-xl">🏓</span>
+                        <p className="font-bold text-gray-200">{conversation.fullName}</p>
+                        <span className="text-xl">{emoji}</span>
                     </div>
                 </div>
             </div>
 
-            <div className="divider my-0 py-0 h-1"></div>
+            {!lastIndex && <div className="divider my-0 py-0 h-1"></div>}
         </>
     );
 }
