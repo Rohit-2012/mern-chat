66673de914ff85619dde6dc8ea3013f69fa7f05a
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-22 10:35:44 +0530

    production build

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import path from 'node:path'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 
@@ -10,6 +11,8 @@ import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 8000
 
+const __dirname = path.resolve()
+
 // MIDDLEWARES
 app.use(express.json())
 app.use(cookieParser())
@@ -18,6 +21,13 @@ app.use(cookieParser())
 app.use("/api/auth", authRouter)
 app.use("/api/messages", messageRouter)
 app.use("/api/users", userRouter)
+
+// STATIC MIDDLEWARE TO USE STATIC FILES
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 server.listen(PORT, () => {
